569e77602926a7581d166d177d22cb468ce17fc4
Rawlston Morton <rawlstonmorton@outlook.com>
2020-07-08 11:34:10 -0400

    my first commit

---
+++ labs/TextHello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TextHello
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name;
+            System.Console.Write("Enter Your Name:");
+            name = System.Console.ReadLine();
+            System.Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
